6e846d004c2254d008c5f12f4095e62c69aac0d2
Manmeet Kaur <MK060@atcltd.atc.org.nz>
2021-05-11 13:57:43 +1200

    Add project files.

---
+++ Migrations/20210511011820_initial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pet_Care_Centre_MVC.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
